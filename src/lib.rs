ae3e52b3073af19885cb5d6daa8353354d756ac8
Jay Lee <busyjaylee@gmail.com>
2019-11-21 15:34:01 +0800

    address comment
    Signed-off-by: Jay Lee <busyjaylee@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 /// choose `Future` as Task, some may just want callbacks, or even Actor
 /// messages. But no matter what a Task is, there should be some role know how
 /// to execute it. The role is call `Runner`.
-/// 
+///
 /// The life cycle of a Runner is:
 /// ```text
 ///   start
@@ -18,37 +18,38 @@
 ///     |
 ///    end
 /// ```
-/// 
+///
 /// Generally users should use the provided future thread pool or callback
-/// thread pool instead. This is only for advance customization. 
+/// thread pool instead. This is only for advance customization.
 pub trait Runner {
     /// The task runner can handle.
     type Task;
+    type Spawn: LocalSpawn<Task = Self::Task>;
 
     /// Called when the runner is started.
-    /// 
+    ///
     /// It's guaranteed to be the first method to called before anything else.
-    fn start(&mut self, _spawn: &mut impl LocalSpawn<Task=Self::Task>) {}
-    
+    fn start(&mut self, _spawn: &mut Self::Spawn) {}
+
     /// Called when a task needs to be handled.
-    /// 
+    ///
     /// It's possible that a task can't be finished in a single execution, in
     /// which case feel free to spawn the task again and return false to
     /// indicate the task has not been finished yet.
-    fn handle(&mut self, spawn: &mut impl LocalSpawn<Task=Self::Task>, task: Self::Task) -> bool;
+    fn handle(&mut self, spawn: &mut Self::Spawn, task: Self::Task) -> bool;
 
     /// Called when the runner is put to sleep.
-    fn pause(&mut self, _spawn: &impl LocalSpawn<Task=Self::Task>) -> bool {
+    fn pause(&mut self, _spawn: &mut Self::Spawn) -> bool {
         true
     }
 
     /// Called when the runner is woken up.
-    fn resume(&mut self, _spawn: &impl LocalSpawn<Task=Self::Task>) {}
+    fn resume(&mut self, _spawn: &mut Self::Spawn) {}
 
     /// Called when the runner is about to be destroyed.
-    /// 
+    ///
     /// It's guaranteed that no other method will be called after this method.
-    fn end(&mut self, _spawn: &impl LocalSpawn<Task=Self::Task>) {}
+    fn end(&mut self, _spawn: &mut Self::Spawn) {}
 }
 
 /// Allows spawn a task to the thread pool from a different thread.
