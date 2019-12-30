944239c5ba47e5b279e183fb87082c50d2018da2
Jay Lee <busyjaylee@gmail.com>
2019-12-30 13:11:57 +0800

    worker: retry poping before going to sleep
    To avoid frequently sleep and wake up, try again poping several times
    before putting thread to sleep.
    
    Signed-off-by: Jay Lee <busyjaylee@gmail.com>

--- src/pool/worker.rs
+++ src/pool/worker.rs
@@ -1,7 +1,8 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
 use crate::pool::{Local, Runner};
-use crate::queue::TaskCell;
+use crate::queue::{Pop, TaskCell};
+use parking_lot_core::SpinWait;
 
 pub(crate) struct WorkerThread<T, R> {
     local: Local<T>,
@@ -19,21 +20,34 @@ where
     T: TaskCell + Send,
     R: Runner<TaskCell = T>,
 {
+    fn slow_pop(&mut self) -> Option<Pop<T>> {
+        // Wait some time before going to sleep, which is more expensive.
+        let mut spin = SpinWait::new();
+        loop {
+            if let Some(t) = self.local.pop() {
+                return Some(t);
+            }
+            if !spin.spin() {
+                break;
+            }
+        }
+        self.runner.pause(&mut self.local);
+        let t = self.local.pop_or_sleep();
+        if t.is_some() {
+            self.runner.resume(&mut self.local);
+        }
+        t
+    }
+
     pub fn run(mut self) {
         self.runner.start(&mut self.local);
         while !self.local.core().is_shutdown() {
             let task = match self.local.pop() {
                 Some(t) => t,
-                None => {
-                    self.runner.pause(&mut self.local);
-                    match self.local.pop_or_sleep() {
-                        Some(t) => {
-                            self.runner.resume(&mut self.local);
-                            t
-                        }
-                        None => continue,
-                    }
-                }
+                None => match self.slow_pop() {
+                    Some(t) => t,
+                    None => continue,
+                },
             };
             self.runner.handle(&mut self.local, task.task_cell);
         }
