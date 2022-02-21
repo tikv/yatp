c7f82583bd08dac229a782a98304a3a23ff03828
Wenbo Zhang <ethercflow@gmail.com>
2022-02-21 20:21:04 +0800

    Add two test cases to test scale_workers (#60)
    Signed-off-by: Wenbo Zhang <ethercflow@gmail.com>

--- src/pool/tests.rs
+++ src/pool/tests.rs
@@ -167,3 +167,124 @@ fn test_shutdown_with_futures() {
         Err(mpsc::RecvTimeoutError::Disconnected)
     );
 }
+
+#[test]
+fn test_scale_up_workers() {
+    let pool = Builder::new("test_scale_up")
+        .max_thread_count(4)
+        .core_thread_count(2)
+        .build_callback_pool();
+
+    let mut sync_txs = vec![];
+    // Block all runnable (`core_thread_count`) threads
+    for _ in 0..2 {
+        let (tx, rx) = mpsc::sync_channel::<()>(0);
+        pool.spawn(move |_: &mut Handle<'_>| rx.recv().unwrap());
+        sync_txs.push(tx);
+    }
+
+    // Make sure all block tasks have been dispatched to `core_thread_count` threads,
+    // as the above block tasks may be pulled to one thread at once, then stealed
+    // by the other thread, which cause the below task is executed first.
+    thread::sleep(Duration::from_secs(1));
+
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    pool.spawn(move |_: &mut Handle<'_>| tx1.send(1).unwrap());
+    // No runnable threads, so it should time out
+    assert_eq!(
+        rx.recv_timeout(Duration::from_secs(1)),
+        Err(mpsc::RecvTimeoutError::Timeout)
+    );
+    // Scale up one worker
+    pool.scale_workers(3);
+    // Due to the current implementation, the scale up cann't be triggered until a new
+    // task has been spawned, so it should still time out
+    assert_eq!(
+        rx.recv_timeout(Duration::from_secs(1)),
+        Err(mpsc::RecvTimeoutError::Timeout)
+    );
+    // Spawn a new task to trigger scale up
+    pool.spawn(move |_: &mut Handle<'_>| tx.send(2).unwrap());
+    // Spawn should success, and handle the two tasks by the spawned order
+    assert_eq!(Ok(1), rx.recv_timeout(Duration::from_secs(1)));
+    assert_eq!(Ok(2), rx.recv_timeout(Duration::from_secs(1)));
+
+    // Block the new scaled thread, to prove only scaled one thread
+    let (tx, rx) = mpsc::sync_channel::<()>(0);
+    pool.spawn(move |_: &mut Handle<'_>| rx.recv().unwrap());
+    sync_txs.push(tx);
+
+    let (tx, rx) = mpsc::channel();
+    pool.spawn(move |_: &mut Handle<'_>| tx.send(1).unwrap());
+    // The scaled thread has been blocked, no more runnable threads, so it should time out
+    assert_eq!(
+        rx.recv_timeout(Duration::from_secs(1)),
+        Err(mpsc::RecvTimeoutError::Timeout)
+    );
+
+    // Wake up the threads that were previously blocked by the task
+    for tx in sync_txs {
+        tx.send(()).unwrap();
+    }
+
+    pool.shutdown();
+}
+
+#[test]
+fn test_scale_down_workers() {
+    let pool = Builder::new("test_scale_down")
+        .max_thread_count(4)
+        .core_thread_count(3)
+        .build_callback_pool();
+
+    let mut sync_txs = vec![];
+    // Block all runnable (`core_thread_count`) threads
+    for _ in 0..3 {
+        let (tx, rx) = mpsc::sync_channel::<()>(0);
+        pool.spawn(move |_: &mut Handle<'_>| rx.recv().unwrap());
+        sync_txs.push(tx);
+    }
+
+    // Make sure all block tasks have been dispatched to `core_thread_count` threads,
+    // as the above block tasks may be pulled by one thread at once, then stealed
+    // by other threads, which will cause the below task is executed first.
+    thread::sleep(Duration::from_secs(1));
+
+    let (tx, rx) = mpsc::channel();
+    pool.spawn(move |_: &mut Handle<'_>| tx.send(1).unwrap());
+    // No runnable threads, so it should time out
+    assert_eq!(
+        rx.recv_timeout(Duration::from_secs(1)),
+        Err(mpsc::RecvTimeoutError::Timeout)
+    );
+
+    // Scale down two workers
+    pool.scale_workers(1);
+    // Due to the current implementation, the scale down cann't be triggered until thread
+    // finished tasks and go to sleep, so wake up them all to finish tasks first.
+    for tx in sync_txs {
+        tx.send(()).unwrap();
+    }
+
+    // Make sure all threads have gone to sleep before spawn a lot of new tasks
+    thread::sleep(Duration::from_secs(1));
+
+    let (tx, rx) = mpsc::channel();
+    // A bunch of tasks should be executed correctly, and as there's only one worker, the
+    // handled order should be the same as spawn order.
+    let cases: Vec<_> = (10..1000).collect();
+    for id in &cases {
+        let t = tx.clone();
+        let id = *id;
+        pool.spawn(move |_: &mut Handle<'_>| t.send(id).unwrap());
+    }
+    let mut ans = vec![];
+    for _ in 10..1000 {
+        let r = rx.recv_timeout(Duration::from_secs(1)).unwrap();
+        ans.push(r);
+    }
+    assert_eq!(cases, ans);
+
+    pool.shutdown();
+}
